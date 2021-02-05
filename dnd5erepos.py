0c0c4945c20326860d187c3c8ae94410bb64bd1e
Alexandre Vale <alexandre.vale@kumulus.com.br>
2021-02-05 17:57:58 -0300

    dial back na complexidade, agora app lista tudo sobre magia

--- dnd5erepos.py
+++ dnd5erepos.py
@@ -1,27 +1,19 @@
 import requests
 
-print("Digite o nome da magia")
-entrada = input()
+def encontremagias():
+    print("Digite o nome da magia")
+    entrada = input()
 
-entrada = entrada.lower()
-entrada = entrada.replace(" ", "-")
+    entrada = entrada.lower()
+    entrada = entrada.replace(" ", "-")
 
 
-print(f'Procurando nos grimorios pela magia {entrada} em https://www.dnd5eapi.co/api/spells/{entrada}/...\n')
-proto = requests.get(f'https://www.dnd5eapi.co/api/spells/{entrada}/')
+    print(f'Procurando nos grimorios pela magia {entrada} em https://www.dnd5eapi.co/api/spells/{entrada}/...\n')
+    proto = requests.get(f'https://www.dnd5eapi.co/api/spells/{entrada}/')
 
-proto = proto.json()
+    proto = proto.json()
 
-if "damage_at_slot_level" in proto:
-    print(f'dano por level de spell slot: {proto["damage"]["damage_at_slot_level"]}')
-else:
-    print(f'dano por level do personagem: {proto["damage"]["damage_at_character_level"]}')
+    for entry in proto:
+        print(f'{entry} : {proto[entry]}\n')
 
-print(proto["damage"]["damage_type"]["name"])
-
-print(f'https://www.dnd5eapi.co/api{proto["damage"]["damage_type"]["url"]}/')
-desc = requests.get(f'https://www.dnd5eapi.co{proto["damage"]["damage_type"]["url"]}/')
-
-desc = desc.json()
-
-print(desc["desc"])
+encontremagias()
